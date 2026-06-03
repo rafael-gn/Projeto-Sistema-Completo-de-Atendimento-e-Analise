409bbd8af5ffb71675a0ff22e10795ccbfc19699
rafael-gn <rafael.gn.066@gmail.com>
2026-06-02 22:35:56 -0300

    implementa estruturas de dados

---
+++ queue.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self.items = deque()
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if self.items:
+            return self.items.popleft()
---
+++ stack.py
@@ -0,0 +1,10 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if self.items:
+            return self.items.pop()
